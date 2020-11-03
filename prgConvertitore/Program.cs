254b7f5b2424f93ad7e1863bf3bd7ed9af7b8e97
FANUX04 <gabry04.gf@gmail.com>
2020-11-03 11:06:46 +0100

    seconda modifica codice che trasforma un numero decimale sia in binario, esadecimale e ottale

--- prgConvertitore/Program.cs
+++ prgConvertitore/Program.cs
@@ -7,21 +7,59 @@ namespace prgConvertitore
         static void Main(string[] args)
         {
             int n;
-            int r;
-            string nconvertito="";
+            int r;         
+            string nconvertito = "";
             int divisore;
             Console.WriteLine("inserisci numero decimale senza virgola:");
             n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("inserisci il divisore (2)");
+            Console.WriteLine($"inserisci il divisore");
             divisore = Convert.ToInt32(Console.ReadLine());
+            
 
-            while(n>0)
+            while (n > 0)
             {
                 r = n % divisore;
-                n = n / 2;
-                nconvertito = Convert.ToString(r) + nconvertito;
+                n = n / divisore;
+                
+
+                if (r == 10)
+                {
+                    nconvertito = "A" + nconvertito;
+                }
+                else if (r == 11)
+                {
+                    nconvertito = "B" + nconvertito;
+                }
+                else if (r == 12)
+                {
+                    nconvertito = "C" + nconvertito;
+                }
+                else if (r == 13)
+                {
+                    nconvertito = "D" + nconvertito;
+
+                }
+                else if (r == 14)
+                {
+                    nconvertito = "E" + nconvertito;
+                }
+                else if(r==15)
+                {
+                    nconvertito = "F" + nconvertito;
+
+                }
+                else
+                {
+                    nconvertito = Convert.ToString(r) + nconvertito;
+                }
+
+
+
             }
+
             Console.WriteLine($"{nconvertito}");
+
+            
         }
     }
 }
