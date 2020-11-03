1a8a405de1e5ec1ab46c7ea02e4307a8f50cc495
FANUX04 <gabry04.gf@gmail.com>
2020-11-03 10:25:47 +0100

    Prima versione del codice

--- prgConvertitore/Program.cs
+++ prgConvertitore/Program.cs
@@ -6,7 +6,22 @@ namespace prgConvertitore
     {
         static void Main(string[] args)
         {
+            int n;
+            int r;
+            string nconvertito="";
+            int divisore;
+            Console.WriteLine("inserisci numero decimale senza virgola:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("inserisci il divisore (2)");
+            divisore = Convert.ToInt32(Console.ReadLine());
 
+            while(n>0)
+            {
+                r = n % divisore;
+                n = n / 2;
+                nconvertito = Convert.ToString(r) + nconvertito;
+            }
+            Console.WriteLine($"{nconvertito}");
         }
     }
 }
